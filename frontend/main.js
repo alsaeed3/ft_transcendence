7713256d50507bba73fa841a3310c07ef892ae83
alsaeed3 <ali.eissa.570@gmail.com>
2025-01-16 14:22:31 +0400

    [frontend]: added main.js

--- frontend/main.js
+++ frontend/main.js
@@ -14,5 +14,19 @@ function displayData(data) {
 	const contentDiv = document.getElementById('content');
 	contentDiv.innerHTML = '';
 
-	if (Array.isArray)
+	if (Array.isArray(data) && data.length > 0) {
+		data.forEach(item => {
+			const itemElement = document.createElement('div');
+			itemElement.className = 'card mb-3';
+			itemElement.innerHTML = `
+				<div class="card-body">
+					<h5 class="card-title">${item.title}</h5>
+					<p class="card-text">${item.content}</p>
+				</div>
+			`;
+			contentDiv.appendChild(itemElement);
+		});
+	} else {
+		contentDiv.innerHTML = `<p>${data.message}</p>`;
+	}
 }
