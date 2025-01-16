fd0c39764cdf5303980a0b0703798dd4687ad9b2
alsaeed3 <ali.eissa.570@gmail.com>
2025-01-16 14:31:13 +0400

    [backend]/[app]: reset the views and urls to be simpler for testing

--- backend/webapp/urls.py
+++ backend/webapp/urls.py
@@ -3,10 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='home'),
-	path('register', views.register, name='register'),
-	path('login', views.login, name='login'),
-	path('logout', views.logout, name='logout'),
-	path('dashboard', views.dashboard, name='dashboard'),
-	path('pong', views.pong, name='pong'),
+	path('api/data/', views.get_data, name='get_data'),
 ]
--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -2,58 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # from .models import Feature
 
 # Create your views here.
-def home(request):
-	return render(request, 'home.html')
-
-def register(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		email = request.POST['email']
-		password = request.POST['password']
-		confirm_password = request.POST['confirm_password']
-
-		if password == confirm_password:
-			if User.objects.filter(email=email).exists():
-				messages.info(request, 'Email already used')
-				return redirect('register')
-			elif User.objects.filter(username=username).exists():
-				messages.info(request, 'Username Already Used')
-				return redirect('register')
-			else:
-				user = User.objects.create_user(username=username, email=email, password=password)
-				user.save()
-				return redirect('login')
-		else:
-			messages.info(request, 'Passwords not matching')
-			return redirect('register')
-	else:
-		return render(request, 'register.html')
-
-def login(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-
-		user = auth.authenticate(username=username, password=password)
-
-		if user is not None:
-			auth.login(request, user)
-			return redirect('dashboard')
-		else:
-			messages.info(request, 'Credentials Invalid')
-			return redirect('login')
-	else:
-		return render(request, 'login.html')
-
-def logout(request):
-	auth.logout(request)
-	return redirect('/')
-
-def dashboard(request):
-	return render(request, 'dashboard.html', {'user': request.user})
-
-def pong(request):
-	return render(request, 'pong.html')
+@api_view(['GET'])
+def get_data(request):
+	data = [
+		{"title": "Item 1", "description": "This is item 1"},
+		{"title": "Item 2", "description": "This is item 2"},
+		{"title": "Item 3", "description": "This is item 3"},
+	]
+	return Response(data)
